87ba1e5c1102d8ed11db5f61856f84e466c1b68e
ShadowOracle19 <lucas.g@hotmail.ca>
2020-12-11 19:22:34 -0500

    First commit

---
+++ Assets/_Scripts/SceneController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public AudioSource click;
+
+    public void OnButtonPressed()
+    {
+        click.Play();
+        SceneManager.LoadScene("Platformer");
+    }
+
+    public void onButtonPressedInstructions()
+    {
+        click.Play();
+        SceneManager.LoadScene("Instructions");
+
+    }
+    public void onButtonPressedMenu()
+    {
+        click.Play();
+        SceneManager.LoadScene("Start");
+
+    }
+}
